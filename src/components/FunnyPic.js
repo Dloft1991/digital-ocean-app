6d88be4391d82a245abe9e3c401f89251fe7138b
DloftonSS <derek.lofton1@gmail.com>
2021-06-14 14:00:18 -0400

    meme

---
+++ src/components/FunnyPic.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import Grossmeme from "../pictures/Grossmeme.jpg";
+export const Fpic = () => {
+  return (
+    <div>
+      <Grossmeme />
+    </div>
+  );
+};
+
+export default Fpic;
